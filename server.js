2de722aada7fccdcb526add7d7598478efeea25c
Akshata Ambi <aksh-ambi@gmail.com>
2019-04-08 17:28:44 +0530

    Adding Webpack & Babel for transpiling

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const path = require('path');
-const express = require('express');
+import path from 'path';
+import express from 'express';
+
 const app = express(),
     DIST_DIR = __dirname,
     HTML_FILE = path.join(DIST_DIR, 'index.html');
 
-app.use(express.static(DIST_DIR))
+app.use(express.static(DIST_DIR));
+
 app.get('*', (req, res) => {
     res.sendFile(HTML_FILE)
-})
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`App listening to ${PORT}....`)
-    console.log('Press Ctrl+C to quit.')
+    console.log(`App listening to ${PORT}....`);
+    console.log('Press Ctrl+C to quit.');
 }) 
